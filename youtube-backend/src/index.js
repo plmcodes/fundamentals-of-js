2c065034a5c97460f184d974b8a9841cde9e8a8e
plmcodes <plmcodes@gmail.com>
2025-11-18 12:31:07 +0530

    utils added

--- youtube-backend/src/index.js
+++ youtube-backend/src/index.js
@@ -10,12 +10,14 @@ dotenv.config({
 
 
 connectDB()
-
-
-
-
-
-
+.then(() => {
+    app.listen(process.env.PORT || 8000 , () => {
+        console.log(`Server is runing at port : ${process.env.PORT}`)
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connecion failed ", err)
+}) 
 
 
 /*
---
+++ youtube-backend/src/utils/ApiError.js
@@ -0,0 +1,23 @@
+class ApiError extends Error {
+    constructor(
+        statusCode,
+        message= "",
+        error = [],
+        stack = ""
+    ) {
+       super(message)
+       this.statusCode = statusCode
+       this.data = null
+       this.messsage = message
+       this.success = false 
+       this.errors = this.errors
+
+       if(stack) {
+          this.stack = stack
+       } else {
+        Error.captureStackTrace(this, this.
+            constructor)
+       }
+    }
+}
+export {ApiError}
